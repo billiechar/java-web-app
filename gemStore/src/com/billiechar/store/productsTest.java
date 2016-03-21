cc14fbda8f29824dd066bea653ec4b707879a1e3
billiechar <chubzx@gmail.com>
2016-03-21 21:11:27 +0000

    updated to use junit
    added junit test that fails

---
+++ gemStore/src/com/billiechar/store/productsTest.java
@@ -0,0 +1,15 @@
+package com.billiechar.store;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class productsTest {
+
+	@Test
+	public void servletShouldReturnJSONObject() {
+		products product = new products();
+		fail("Not yet implemented");
+	}
+
+}
